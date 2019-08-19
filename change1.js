95fae809fea4028e880710dfe134b2a09ececfdb
ShaneLin <shane.sandy123@gmail.com>
2019-08-19 15:59:56 +0800

    recommit5-2

--- change1.js
+++ change1.js
@@ -4,3 +4,4 @@ var change2 = `practice2: recommit3 again`;
 var change3 = `practice3: recommit1 again`;
 var change4 = `practice4: recommit1 again`;
 var stash = `practice5: stash something`;
+var change5 = `practice5: recommit1 again`;
