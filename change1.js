d57e4f22b1dc28561ece5fb5b9392ce6fdd3fbd3
Shane Lin <yushanelin@redso.com.hk>
2021-09-01 14:25:29 +0800

    test: new line

--- change1.js
+++ change1.js
@@ -4,3 +4,5 @@ var change2 = `practice2: recommit3 again`;
 var change3 = `practice3: recommit1 again`;
 var change4 = `practice4: recommit1 again`;
 var stash = `practice5: stash-dev`;
+
+console.log("new line");
